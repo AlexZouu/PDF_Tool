4f374a0fe7d29c594ff8d84554bdc46be9a94223
Alex Zou <alexzou26@gmail.com>
2026-09-22 15:36:58 -0400

    Making the UI look nicer

--- src/main.py
+++ src/main.py
@@ -33,7 +33,7 @@ def main():
   root.bind_all('<Button-1>', lambda event: event.widget.focus_set())
   # root.resizable(False, False)
   root.title('PDF Tool')
-  root.geometry('500x365')
+  root.geometry('500x345')
   
   root.columnconfigure(0, weight=1)
   root.columnconfigure(1, weight=0)
@@ -41,36 +41,41 @@ def main():
   root.columnconfigure(3, weight=0)
   root.columnconfigure(4, weight=1)
 
-  title_label = tk.Label(root, text='PDF Tool', font=('Segoe UI', 16))
-  # explanatory_text_label = tk.Label(root, text=config['explanatoryText'], wraplength=425, font=('Segoe UI', 12), justify='left')
+  title_label = tk.Label(root, text='PDF Tool', justify='center', font=('Segoe UI', 16))
   title_label.grid(row=0, column=2, pady=(20, 10))
-  # explanatory_text_label.grid(row=1, column=1, columnspan=3, pady=(0, 10), sticky='w')
 
-  page_offset_description_label = tk.Label(root, text='Which page of the PDF corresponds to the page numbered as 1 in the textbook', font=('Segoe UI', 10))
-  page_offset_description_label.grid(row=1, column=1, columnspan=3)
+  page_offset_description_label = tk.Label(root, text='Please enter the page offset.\nThe page offset is the page of the PDF that corresponds to the page numbered as 1 in the textbook.', wraplength=425, justify='center', font=('Segoe UI', 10))
+  page_offset_description_label.grid(row=1, column=1, columnspan=3, sticky='n')
 
-  page_offset_label = tk.Label(root, text='Page offset: ', font=('Segoe UI', 10))
-  page_offset_label.grid(row=2, column=1, pady=(10, 0))
+  page_offset_frame = tk.Frame(root)
+  page_offset_frame.grid(row=2, column=1, columnspan=3)
+
+  page_offset_label = tk.Label(page_offset_frame, text='Page offset: ', font=('Segoe UI', 10), justify='right')
+  page_offset_label.grid(row=2, column=1, pady=(10, 0), sticky='e')
 
   offset = tk.IntVar(value=cache.retrieve_page_offset())
-  page_offset_entry = tk.Entry(root, textvariable=offset, validate='key', validatecommand=(vint, '%P'), font=('Segoe UI', 10))
-  page_offset_entry.grid(row=2, column=2, pady=(10, 0), ipadx=2, ipady=2)
+  page_offset_entry = tk.Entry(page_offset_frame, textvariable=offset, validate='key', validatecommand=(vint, '%P'), width=5, justify='center', font=('Segoe UI', 10))
+  page_offset_entry.grid(row=2, column=2, pady=(10, 0), ipadx=2, ipady=2, sticky='w')
 
-  page_numbers_description_label = tk.Label(root, text='For page ranges, enter the range in the format of "<start>-<end>". For single pages, please enter each individual page number. Please separate all page ranges and numbers with a comma.', font=('Segoe UI', 10))
+  page_numbers_description_label = tk.Label(root, text='Please enter which pages you would like to keep.\nFor page ranges, enter the range in the format of "<start>-<end>".\nFor single pages, please enter each individual page number.\nPlease separate all page ranges and numbers with a comma.', wraplength=425, justify='center', font=('Segoe UI', 10))
   page_numbers_description_label.grid(row=3, column=1, columnspan=3, pady=(10,0))
 
-  page_numbers_label = tk.Label(root, text='Page range: ', font=('Segoe UI', 10))
-  page_numbers_label.grid(row=4, column=1, pady=(10, 0))
+  page_numbers_frame = tk.Frame(root)
+  page_numbers_frame.grid(row=4, column=1, columnspan=3)
+
+  page_numbers_label = tk.Label(page_numbers_frame, text='Pages to keep: ', font=('Segoe UI', 10), justify='right')
+  page_numbers_label.grid(row=4, column=1, pady=(10, 10), sticky='e')
 
   page_numbers = tk.StringVar()
-  page_numbers_entry = tk.Entry(root, textvariable=page_numbers, validate='key', validatecommand=(vrange, '%P'), font=('Segoe UI', 10))
-  page_numbers_entry.grid(row=4, column=2, columnspan=2, pady=(10, 0), ipadx=2, ipady=2)
+  page_numbers_entry = tk.Entry(page_numbers_frame, textvariable=page_numbers, validate='key', validatecommand=(vrange, '%P'), width=40, justify='left', font=('Segoe UI', 10))
+  page_numbers_entry.grid(row=4, column=2, columnspan=2, pady=(10, 10), ipadx=2, ipady=2, sticky='w')
 
   choose_pdf_button = tk.Button(
       root, 
       text='Choose PDF', 
       command=lambda: choose_pdf(root, offset.get(), page_numbers.get()), 
-      font=('Segoe UI', 8),
+      font=('Segoe UI', 10),
+      justify='center'
   )
   choose_pdf_button.grid(row=5, column=2)
 
